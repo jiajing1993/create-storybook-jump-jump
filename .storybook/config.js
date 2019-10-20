d4defebd3f80a8d335218ce3cd943ac60d0c11b0
Jia Jing <jiajing19932@gmail.com>
2019-10-20 18:25:45 +0800

    feat: add snapshot testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,14 @@
 import { configure } from '@storybook/react';
 import '../src/index.css';
+import requireContext from 'require-context.macro';
+// babel-plugin-macros defines a standard interface for libraries that want to use compile-time code
+// transformation without requiring the user to add a babel plugin to their build system
+// (other than babel-plugin-macros, which is ideally already in place).
 
 // automatically import all files ending in *.stories.js
 // configure(require.context('../src/stories', true, /\.stories\.js$/), module);
 
-const req = require.context('../src', true, /\.stories.js$/);
+const req = requireContext('../src', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
